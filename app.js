1cd0896f7d7171cb1b547cf569315f2d2611a2a5
Danny <dannybanko@gmail.com>
2021-02-06 19:04:20 -0800

    sticky navbar and rough education

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const mainImage = document.querySelector('.mainImage');
 const slider = document.querySelector('.slider');
 const logo = document.querySelector('.logo');
 const navLinks = document.querySelector('.nav-links');
-const myName = document.querySelector('.myName');
 const nav = document.querySelector('#nav');
 const footer = document.querySelector('#footer');
 
@@ -13,6 +12,5 @@ tl.fromTo(mainImage, 1, {height: "0%"}, {height: "80%", ease: Power2.easeInOut }
 .fromTo(slider, 1.2, {x: "-100%"}, {x: "0%", ease: Power2.easeInOut }, "-=1.2")
 .fromTo(logo, 0.5, {opacity: 0, x: 30}, {opacity: 1, x: 0}, "-=.5")
 .fromTo(navLinks, 0.5, {opacity: 0, x: 30}, {opacity: 1, x: 0}, "-=.5")
-.fromTo(nav, 0.5, {opacity: 0, x: 30}, {opacity: .7, x: 0}, "-=.5")
-.fromTo(footer, 0.5, {opacity: 0, x: 30}, {opacity: .7, x: 0}, "-=.5")
-.fromTo(myName, 0.5, {opacity: 0, x: 30}, {opacity: .7, x: 0}, "-=.5");
+.fromTo(nav, 0.5, {opacity: 0, y: -30}, {opacity: .7, y: 0}, "-=.5")
+.fromTo(footer, 0.5, {opacity: 0, x: 30}, {opacity: .7, x: 0}, "-=.5");
