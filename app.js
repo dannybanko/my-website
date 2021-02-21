686f36b20891cbee62a5df35b24227252cb8db27
Danny <dannybanko@gmail.com>
2021-02-21 14:12:33 -0800

    more JS and content

--- app.js
+++ app.js
@@ -14,3 +14,8 @@ tl.fromTo(mainImage, 1, {height: "0%"}, {height: "80%", ease: Power2.easeInOut }
 .fromTo(navLinks, 0.5, {opacity: 0, x: 30}, {opacity: 1, x: 0}, "-=.5")
 .fromTo(nav, 0.5, {opacity: 0, y: -30}, {opacity: .7, y: 0}, "-=.5")
 .fromTo(footer, 0.5, {opacity: 0, x: 30}, {opacity: .7, x: 0}, "-=.5");
+
+const overview = document.querySelector('.overview');
+
+const t2 = new TimelineMax();
+t2.fromTo(overview, 1, {height: "0%"}, {height: "80%", ease: Power2.easeInOut});
